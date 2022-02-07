d06b0294d259f93a28f1f2848561e34de6e2b431
dmncp <damiancyper@gmail.com>
2022-02-07 19:28:36 +0100

    fixed example

--- examples/example2/script.js
+++ examples/example2/script.js
@@ -4,7 +4,7 @@ let columnChartExample2 = new ColumnChart(
         canvas: chart2,
         dataValues: [845, 614, 539, 282, 175],
         dataList: ["RTV i TV", "Komputery", "Kamery", "Konsole i gry", "AGD"],
-        title: "Sprzedarz towarów w firmie X w 2020r (w sztukach)"
+        title: "Sprzedaż towarów w firmie X w 2020r (w sztukach)"
     }
 );
 columnChartExample2.render();
